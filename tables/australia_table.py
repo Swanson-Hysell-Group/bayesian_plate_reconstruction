32d19de385ec7d3f666a355644fad9a6e4744b10
Ian Rose <ian.r.rose@gmail.com>
2016-08-10 08:45:10 -0700

    Add units to ages in Australia table

--- tables/australia_table.py
+++ tables/australia_table.py
@@ -9,15 +9,15 @@ df.fillna('', inplace=True)
 df.rename(columns={'ROCKNAME': 'Pole',
                    'PLAT': '$\\psi_p$', 
                    'PLONG':'$\\phi_p$', 
-                   'LOMAGAGE': 'Lower age',
-                   'HIMAGAGE': 'Upper age'}, inplace=True)
+                   'LOMAGAGE': 'Lower age (Ma)',
+                   'HIMAGAGE': 'Upper age (Ma)'}, inplace=True)
 
-df.sort_values(by='Upper age', inplace=True)
+df.sort_values(by='Upper age (Ma)', inplace=True)
 
 df['$A_{95}$'] = np.round(np.sqrt(df['DP']*df['DM']), decimals=1)
 
 with open("australia_poles.tex", 'w') as f:
     df[['Pole', '$\\psi_p$', '$\\phi_p$', '$A_{95}$', 'Reference',\
-        'Lower age', 'Upper age']].to_latex(f, 
+        'Lower age (Ma)', 'Upper age (Ma)']].to_latex(f, 
          float_format="%g", escape=False, longtable=False, index=False,
          column_format='p{4cm} p{1.0cm} p{1.0cm} p{1.0cm} p{2.5cm} p{1cm} p{1cm}')
